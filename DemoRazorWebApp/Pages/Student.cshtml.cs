b46780cddfa53ee4b51c3c3e6fadd5b415ba8bbf
Praveen Nayak <pnayak@hamiltongroupuk.com>
2022-03-22 15:35:26 +0000

    Add project files.

---
+++ DemoRazorWebApp/Pages/Student.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoRazorWebApp.Pages
+{
+    public class StudentModel : PageModel
+    {
+        public string Name { get; set; }
+        public void OnGet()
+        {
+            Name = "Mr Bean";
+        }
+
+
+    }
+}
